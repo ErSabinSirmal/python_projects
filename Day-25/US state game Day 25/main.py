28f96f6d0294aa71543fa22e62bb1d0e0afe276d
Sabin Sirmal <sabinsir12345@gmail.com>
2024-09-24 16:32:38 +0545

    day 25 pandas workout

---
+++ Day-25/US state game Day 25/main.py
@@ -0,0 +1,47 @@
+import turtle
+from importlib.machinery import all_suffixes
+
+import pandas
+
+
+screen = turtle.Screen()
+my_turtle = turtle.Turtle()
+screen.title("U.S States Game")
+image = "blank_states_img.gif"
+screen.addshape(image) # used to add the image as the background of the turtle screen..
+turtle.shape(image)
+
+
+data = pandas.read_csv("50_states.csv")
+
+all_states = data.state.to_list()
+guessed_states = []
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title = f"{len(guessed_states)}/50", prompt= "What's another state's name?").title()
+    # print(answer_state)
+    if answer_state == "Exit":
+        # Generating the CSV file for the missed states
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        # print(state_name)
+        # cor_x = (state_name["x"].item()) # when only state_name["x"] is written then it give both index
+        # # and the co-ordinate which is not suitable for goto method so we need item()
+        # cor_y = (state_name["y"].item())
+        # print(f"{cor_x}, {cor_y}")
+        # title_answer = answer_state.title()
+        # print(type(cor_x))
+        state_data = data[data.state == answer_state]
+
+        my_turtle.penup()
+        my_turtle.hideturtle()
+        my_turtle.goto(state_data.x.item(), state_data.y.item())
+        my_turtle.write(f"{answer_state}", font=("Arial", 8, "normal"))
+
+
---
+++ Day-25/main.py
@@ -0,0 +1,94 @@
+# # with open("weather_data.csv") as data:
+# #     weather_report = data.readlines()
+# # print("List of name is: ")
+# # print(weather_report)
+#
+#
+# # We can import csv to print the list in the readable format
+#
+# # import csv
+# #
+# # with open("weather_data.csv") as data_files:
+# #     data = csv.reader(data_files)
+# #     temperature = []
+# #     for row in data:
+# #         if row[1] != "temp":
+# #             temperature.append(int(row[1]))
+# #     print(temperature)
+#
+#     # for item in temperature[1:]:
+#     #     tempr = int(item)
+#     #     print(tempr)
+#
+# import pandas
+#
+# data = pandas.read_csv("weather_data.csv")
+# # print(data["temp"])
+#  # dataframe conversion
+# # print((data["temp"]))
+# # data_dict = data.to_dict()
+# # print(data_dict)
+# #
+# # temp_list = data["temp"].to_list()
+# # print(len(temp_list))
+# # print(temp_list)
+# #
+# # # average_tempr = sum(temp_list) / len(temp_list)
+# # # print(average_tempr)
+# #
+# # print(data["temp"].mean())
+# # print(data["temp"].max())
+# #
+# # # Get Data in columns
+# # print(data["condition"])
+# # print(data.condition)
+#
+# #get data in Row
+#
+# # print(data[data.day == "Monday"])
+# # maximum = data.temp.max()
+# # print(data[data.temp == maximum])
+#
+# # tuesday = data[data.day == "Tuesday"]
+# # print(tuesday.condition)
+#
+# # tempreture = data[data.day == "Monday"]
+# # temp_fahrenheit = (tempreture.temp * 9/5) + 32
+# # print(temp_fahrenheit)
+#
+# # create a dataframe from scratch
+#
+# data_dict  = {
+#     "students": ["Amy", "James", "Angela"],
+#     "scores": [76, 56, 65]
+# }
+#
+# my_data = pandas.DataFrame(data_dict)
+# print(my_data)
+#
+# #converting the dataframe to the csv file
+# my_data.to_csv("new_data.csv")
+# squirrel census data analysis...
+
+import pandas as pd
+
+data = pd.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+grey_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
+red_squirrels_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrels_count = len(data[data["Primary Fur Color"] == "Black"])
+
+print(grey_squirrels_count)
+print(red_squirrels_count)
+print(black_squirrels_count)
+
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [grey_squirrels_count, red_squirrels_count, black_squirrels_count]
+}
+
+my_squirrels = pd.DataFrame(data_dict)
+# print(my_squirrels)
+my_squirrels.to_csv("squirrel_count.csv")
+
+
+
